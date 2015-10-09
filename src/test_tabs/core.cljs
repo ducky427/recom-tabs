d9778bb9d5639cc9264233167fac7cc9dc9e6c3e
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2015-10-09 13:15:51 +0100

    Some cleanup

--- src/test_tabs/core.cljs
+++ src/test_tabs/core.cljs
@@ -1,6 +1,5 @@
 (ns ^:figwheel-always test-tabs.core
-  (:require [clojure.set :as cset]
-            [reagent.core :as reagent]
+  (:require [reagent.core :as reagent]
             [re-com.core :as rcc]))
 
 (enable-console-print!)
@@ -11,13 +10,12 @@
 
 (defn Tabs
   [tabs model on-change]
-  (let [ids     (map :id tabs)
-        states  (reagent/atom (into {} (map
-                                        (fn [x]
-                                          (if (= model x)
-                                            {x "active in"}
-                                            {x ""}))
-                                        ids)))]
+  (let [states  (reagent/atom (into {} (map
+                                        (fn [{x :id}]
+                                          [x (if (= model x)
+                                               "active in"
+                                               "")])
+                                        tabs)))]
     (fn [tabs model on-change]
       [rcc/v-box
        :children [[rcc/horizontal-tabs
@@ -49,9 +47,9 @@
               [Tabs
                [{:id :a :label "A" :content [:p "Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui."]}
                 {:id :b :label "B" :content [:p "Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park."]}]
-   @app-state
-   (fn [x]
-     (reset! app-state x))]]])
+               @app-state
+               (fn [x]
+                 (reset! app-state x))]]])
 
 (reagent/render-component [hello-world] (. js/document (getElementById "app")))
 
